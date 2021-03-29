dda9eaac535b0ea4329da70ce1bb71991301da10
veegishx <veegish240@gmail.com>
2021-03-29 21:10:56 +0400

    Added Field Widget for post spectre headers

---
+++ src/Constant/PostSpectreType.php
@@ -0,0 +1,10 @@
+<?php
+namespace Drupal\post_spectre\Constant;
+
+class PostSpectreType
+{
+    const OPEN_CROSS_ORIGIN_WINDOW = 'cow';
+    const CROSS_ORIGIN_OPENERS = 'coo';
+    const CROSS_ORIGIN_OPENERS_IFRAME = 'coo_iframe';
+    const CROSS_ORIGIN_OPENERS_POPUP = 'coo_popup';
+}
